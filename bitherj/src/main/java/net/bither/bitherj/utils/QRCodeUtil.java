8f2c3ff513edb5bfac04a678ba1e2188562f3c83
jjz <woaf1003@gmail.com>
2014-09-20 14:35:58 +0800

    upgrade verify qrcodeVersion

--- bitherj/src/main/java/net/bither/bitherj/utils/QRCodeUtil.java
+++ bitherj/src/main/java/net/bither/bitherj/utils/QRCodeUtil.java
@@ -90,13 +90,15 @@ public class QRCodeUtil {
     public static boolean verifyQrcodeTransport(String text) {
         Pattern pattern = Pattern.compile("[^0-9A-Z/+]");
         Matcher matcher = pattern.matcher(text);
+        boolean verifyNewVersion = true;
+        boolean verifyOldVersion = true;
         if (matcher.find()) {
-            return false;
+            verifyNewVersion = false;
         }
         if (!oldVerifyQrcodeTransport(text)) {
-            return false;
+            verifyOldVersion = false;
         }
-        return true;
+        return verifyNewVersion || verifyOldVersion;
     }
 
     public static List<String> getQrCodeStringList(String str) {
