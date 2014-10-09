fad9f1482a193e0213ced359932929591106df78
jjz <woaf1003@gmail.com>
2014-10-09 10:06:22 +0800

    remove AppAdroidImpl

--- bitherj/src/main/java/net/bither/bitherj/BitherjApplication.java
+++ bitherj/src/main/java/net/bither/bitherj/BitherjApplication.java
@@ -51,7 +51,6 @@ public abstract class BitherjApplication extends Application {
         mDbHelper = new BitherjDatabaseHelper(mContext);
         super.onCreate();
         App.notificationService.removeAddressLoadCompleteState();
-        initApp();
     }
 
     @Override
@@ -61,70 +60,7 @@ public abstract class BitherjApplication extends Application {
     }
 
 
-    public abstract ISetting initSetting();
 
-    public abstract IRandom initRandom();
 
-    public static File getLogDir() {
-        final File logDir = BitherjApplication.mContext.getDir("log", Context.MODE_WORLD_READABLE);
-        return logDir;
-    }
 
-    private void initLogging() {
-        final File logDir = getLogDir();
-        final File logFile = new File(logDir, "bitherj.log");
-        final LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        final PatternLayoutEncoder filePattern = new PatternLayoutEncoder();
-        filePattern.setContext(context);
-        filePattern.setPattern("%d{HH:mm:ss.SSS} [%thread] %logger{0} - %msg%n");
-        filePattern.start();
-
-        final RollingFileAppender<ILoggingEvent> fileAppender = new
-                RollingFileAppender<ILoggingEvent>();
-        fileAppender.setContext(context);
-        fileAppender.setFile(logFile.getAbsolutePath());
-
-        final TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new
-                TimeBasedRollingPolicy<ILoggingEvent>();
-        rollingPolicy.setContext(context);
-        rollingPolicy.setParent(fileAppender);
-        rollingPolicy.setFileNamePattern(logDir.getAbsolutePath() + "/bitherj.%d.log.gz");
-        rollingPolicy.setMaxHistory(7);
-        rollingPolicy.start();
-
-        fileAppender.setEncoder(filePattern);
-        fileAppender.setRollingPolicy(rollingPolicy);
-        fileAppender.start();
-
-        final PatternLayoutEncoder logcatTagPattern = new PatternLayoutEncoder();
-        logcatTagPattern.setContext(context);
-        logcatTagPattern.setPattern("%logger{0}");
-        logcatTagPattern.start();
-
-        final PatternLayoutEncoder logcatPattern = new PatternLayoutEncoder();
-        logcatPattern.setContext(context);
-        logcatPattern.setPattern("[%thread] %msg%n");
-        logcatPattern.start();
-
-        final LogcatAppender logcatAppender = new LogcatAppender();
-        logcatAppender.setContext(context);
-        logcatAppender.setTagEncoder(logcatTagPattern);
-        logcatAppender.setEncoder(logcatPattern);
-        logcatAppender.start();
-
-        final ch.qos.logback.classic.Logger log = context.getLogger(Logger.ROOT_LOGGER_NAME);
-        log.addAppender(fileAppender);
-        log.addAppender(logcatAppender);
-        log.setLevel(Level.INFO);
-    }
-
-    private void initApp() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                AddressManager.getInstance();
-                initLogging();
-            }
-        }).start();
-    }
 }
