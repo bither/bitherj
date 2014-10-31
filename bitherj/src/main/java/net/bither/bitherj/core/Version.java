5e71f7963f005ffc33c6e13b75831d3ced18f606
ZhouQi <bitwolaiye@gmail.com>
2014-10-31 11:23:32 +0800

    version 1.2.0->1.2.1

--- bitherj/src/main/java/net/bither/bitherj/core/Version.java
+++ bitherj/src/main/java/net/bither/bitherj/core/Version.java
@@ -17,6 +17,6 @@
 package net.bither.bitherj.core;
 
 public class Version {
-    public static final String version = "1.2.0";
+    public static final String version = "1.2.1";
     public static final String name = "Bitherj";
 }
