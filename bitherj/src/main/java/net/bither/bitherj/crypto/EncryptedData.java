42c138f7924b9aea05459983bdf04323056be359
jjz <woaf1003@gmail.com>
2015-01-05 12:31:09 +0800

    init EncryptedData

--- bitherj/src/main/java/net/bither/bitherj/crypto/EncryptedData.java
+++ bitherj/src/main/java/net/bither/bitherj/crypto/EncryptedData.java
@@ -1,18 +1,31 @@
 package net.bither.bitherj.crypto;
 
-/**
- * Created by songchenwen on 15/1/4.
- */
+import net.bither.bitherj.qrcode.QRCodeUtil;
+import net.bither.bitherj.utils.Utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EncryptedData {
+
+    private static final Logger log = LoggerFactory.getLogger(EncryptedData.class);
+
     private byte[] encryptedData;
     private byte[] initialisationVector;
     private byte[] salt;
 
-    public EncryptedData(String str){
-        //TODO from encrypted string
+    public EncryptedData(String str) {
+        String[] strs = QRCodeUtil.splitOfPasswordSeed(str);
+        if (strs.length != 3) {
+            log.error("decryption: EncryptedData format error");
+        }
+        initialisationVector = Utils.hexStringToByteArray
+                (strs[1]);
+        encryptedData = Utils.hexStringToByteArray(strs[0]);
+        salt = Utils.hexStringToByteArray(strs[2]);
     }
 
-    public EncryptedData(byte[] dataToEncrypt, CharSequence password){
+    public EncryptedData(byte[] dataToEncrypt, CharSequence password) {
         KeyCrypterScrypt crypter = new KeyCrypterScrypt();
         salt = crypter.getSalt();
         EncryptedPrivateKey k = crypter.encrypt(dataToEncrypt, crypter.deriveKey(password));
@@ -20,12 +33,12 @@ public class EncryptedData {
         initialisationVector = k.getInitialisationVector();
     }
 
-    public byte[] decrypt(CharSequence password){
+    public byte[] decrypt(CharSequence password) {
         KeyCrypterScrypt crypter = new KeyCrypterScrypt(salt);
         return crypter.decrypt(new EncryptedPrivateKey(initialisationVector, encryptedData), crypter.deriveKey(password));
     }
 
-    public String toString(){
+    public String toString() {
         //TODO to string
         return null;
     }
