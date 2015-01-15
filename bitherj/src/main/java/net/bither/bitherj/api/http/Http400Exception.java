5b50c39149db1320938b4f93a22427525a523d2a
jjz <woaf1003@gmail.com>
2015-01-15 15:48:00 +0800

    throw http400Exception

--- bitherj/src/main/java/net/bither/bitherj/api/http/Http400Exception.java
+++ bitherj/src/main/java/net/bither/bitherj/api/http/Http400Exception.java
@@ -17,26 +17,32 @@
 package net.bither.bitherj.api.http;
 
 public class Http400Exception extends HttpException {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private int errorCode;
 
-	public Http400Exception(Exception cause) {
-		super(cause);
-	}
+    public Http400Exception(Exception cause) {
+        super(cause);
+    }
 
-	public Http400Exception(String msg, int statusCode) {
-		super(msg, statusCode);
-	}
+    public Http400Exception(int errorCode, String msg) {
+        super(msg);
+        this.errorCode = errorCode;
+    }
 
-	public Http400Exception(String msg, Exception cause, int statusCode) {
-		super(msg, cause, statusCode);
+    public Http400Exception(String msg, Exception cause, int statusCode) {
+        super(msg, cause, statusCode);
 
-	}
+    }
 
-	public Http400Exception(String msg, Exception cause) {
-		super(msg, cause);
-	}
+    public Http400Exception(String msg, Exception cause) {
+        super(msg, cause);
+    }
 
-	public Http400Exception(String msg) {
-		super(msg);
-	}
+    public Http400Exception(String msg) {
+        super(msg);
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
 }
--- bitherj/src/main/java/net/bither/bitherj/api/http/HttpsPostResponse.java
+++ bitherj/src/main/java/net/bither/bitherj/api/http/HttpsPostResponse.java
@@ -1,5 +1,7 @@
 package net.bither.bitherj.api.http;
 
+import org.json.JSONObject;
+
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,10 +15,9 @@ import java.util.Map;
 public abstract class HttpsPostResponse<T> extends BaseHttpsResponse<T> {
 
     public void handleHttpPost() throws Exception {
-        HttpsURLConnection url_con = null;
+        HttpsURLConnection con = null;
         String responseContent = null;
         try {
-
             StringBuffer params = new StringBuffer();
             for (Iterator iter = getParams().entrySet().iterator(); iter
                     .hasNext(); ) {
@@ -33,42 +34,59 @@ public abstract class HttpsPostResponse<T> extends BaseHttpsResponse<T> {
             }
 
             URL url = new URL(getUrl());
-            url_con = (HttpsURLConnection) url.openConnection();
-            url_con.setRequestMethod("POST");
+            con = (HttpsURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
             System.setProperty("sun.net.client.defaultConnectTimeout", String
                     .valueOf(HttpSetting.HTTP_CONNECTION_TIMEOUT));
             System.setProperty("sun.net.client.defaultReadTimeout", String
                     .valueOf(HttpSetting.HTTP_SO_TIMEOUT));
 
-            url_con.setDoOutput(true);
+            con.setDoOutput(true);
             byte[] b = params.toString().getBytes();
-            url_con.getOutputStream().write(b, 0, b.length);
-            url_con.getOutputStream().flush();
-            url_con.getOutputStream().close();
+            con.getOutputStream().write(b, 0, b.length);
+            con.getOutputStream().flush();
+            con.getOutputStream().close();
 
-            InputStream in = url_con.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(in,
-                    HttpSetting.REQUEST_ENCODING));
-            String tempLine = rd.readLine();
-            StringBuffer tempStr = new StringBuffer();
-            String crlf = System.getProperty("line.separator");
-            while (tempLine != null) {
-                tempStr.append(tempLine);
-                tempStr.append(crlf);
-                tempLine = rd.readLine();
-            }
-            responseContent = tempStr.toString();
-            rd.close();
+            InputStream in = con.getInputStream();
+            responseContent = getStringFromIn(in);
+
             in.close();
         } catch (IOException e) {
             e.printStackTrace();
-            throw e;
+            if (con.getResponseCode() == 400) {
+                String str = getStringFromIn(con.getErrorStream());
+                JSONObject json = new JSONObject(str);
+                Iterator it = json.keys();
+                if (it.hasNext()) {
+                    String key = (String) it.next();
+                    String value = json.getString(key);
+                    throw new Http400Exception(Integer.valueOf(key), value);
+                }
+            } else {
+                throw e;
+            }
         } finally {
-            if (url_con != null) {
-                url_con.disconnect();
+            if (con != null) {
+                con.disconnect();
             }
         }
         setResult(responseContent);
+    }
+
+    private String getStringFromIn(InputStream in) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(in,
+                HttpSetting.REQUEST_ENCODING));
+        String tempLine = rd.readLine();
+        StringBuffer tempStr = new StringBuffer();
+        String crlf = System.getProperty("line.separator");
+        while (tempLine != null) {
+            tempStr.append(tempLine);
+            tempStr.append(crlf);
+            tempLine = rd.readLine();
+        }
+        rd.close();
+
+        return tempStr.toString();
     }
 
     public abstract Map<String, String> getParams() throws Exception;
