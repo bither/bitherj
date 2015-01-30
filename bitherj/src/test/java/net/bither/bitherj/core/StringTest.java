74e944c6f8a800dc6fbd9280df28b6cf255c1f40
jjz <woaf1003@gmail.com>
2015-01-30 18:18:21 +0800

    rename

--- bitherj/src/test/java/net/bither/bitherj/core/StringTest.java
+++ bitherj/src/test/java/net/bither/bitherj/core/StringTest.java
@@ -10,11 +10,11 @@ public class StringTest {
     @Test
     public void testString() {
         boolean result =
-                QRCodeUtil.verifyQrcodeTransport("-E0B56EB20152755D3287BEBAAB612BB4049E736A44301729D878D54CA0912DF84F88BBDBE7330CA412FF700991BE8FE1/989A374A3B0F301808654DCD6264F368/01f120397c1017c628");
+                QRCodeUtil.verifyBitherQRCode("-E0B56EB20152755D3287BEBAAB612BB4049E736A44301729D878D54CA0912DF84F88BBDBE7330CA412FF700991BE8FE1/989A374A3B0F301808654DCD6264F368/01f120397c1017c628");
         if (result) {
-            System.out.println("verifyQrcodeTransport pass");
+
         } else {
-            System.out.println("verifyQrcodeTransport no pass");
+
         }
     }
 }
