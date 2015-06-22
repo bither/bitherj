ec38720a158b9f6a56f6c41f11ceff6535570648
songchenwen <sanvinsung@gmail.com>
2015-06-22 15:45:24 +0800

    Add HD Account Cold

--- bitherj/src/main/java/net/bither/bitherj/core/HDAccountCold.java
+++ bitherj/src/main/java/net/bither/bitherj/core/HDAccountCold.java
@@ -18,17 +18,118 @@
 
 package net.bither.bitherj.core;
 
+import net.bither.bitherj.crypto.ECKey;
+import net.bither.bitherj.crypto.EncryptedData;
+import net.bither.bitherj.crypto.hd.DeterministicKey;
+import net.bither.bitherj.crypto.hd.HDKeyDerivation;
+import net.bither.bitherj.crypto.mnemonic.MnemonicException;
+import net.bither.bitherj.db.AbstractDb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by songchenwen on 15/6/19.
  */
 public class HDAccountCold extends AbstractHD {
+
+    private static final Logger log = LoggerFactory.getLogger(HDAccountCold.class);
+
+    public HDAccountCold(byte[] mnemonicSeed, CharSequence password, boolean isFromXRandom)
+            throws MnemonicException.MnemonicLengthException {
+        this.mnemonicSeed = mnemonicSeed;
+        hdSeed = seedFromMnemonic(mnemonicSeed);
+        this.isFromXRandom = isFromXRandom;
+        DeterministicKey master = HDKeyDerivation.createMasterPrivateKey(hdSeed);
+        EncryptedData encryptedHDSeed = new EncryptedData(hdSeed, password, isFromXRandom);
+        EncryptedData encryptedMnemonicSeed = new EncryptedData(mnemonicSeed, password,
+                isFromXRandom);
+        String firstAddress;
+        ECKey k = new ECKey(mnemonicSeed, null);
+        String address = k.toAddress();
+        k.clearPrivateKey();
+        DeterministicKey accountKey = getAccount(master);
+        DeterministicKey externalKey = getChainRootKey(accountKey, AbstractHD.PathType
+                .EXTERNAL_ROOT_PATH);
+        DeterministicKey key = externalKey.deriveSoftened(0);
+        firstAddress = key.toAddress();
+        accountKey.wipe();
+        master.wipe();
+        wipeHDSeed();
+        wipeMnemonicSeed();
+        hdSeedId = 0;//TODO AbstractDb.addressProvider.addHDAccount(encryptedMnemonicSeed
+        // .toEncryptedString(), encryptedHDSeed.toEncryptedString(), firstAddress,
+        // isFromXRandom, address);
+        externalKey.wipe();
+    }
+
+    public HDAccountCold(byte[] mnemonicSeed, CharSequence password) throws MnemonicException
+            .MnemonicLengthException {
+        this(mnemonicSeed, password, false);
+    }
+
+    public HDAccountCold(SecureRandom random, CharSequence password) throws MnemonicException
+            .MnemonicLengthException {
+        this(randomByteFromSecureRandom(random, 16), password, random.getClass().getCanonicalName
+                ().indexOf("XRandom") >= 0);
+    }
+
+    public HDAccountCold(int hdSeedId) {
+        this.hdSeedId = hdSeedId;
+        this.isFromXRandom = false;// TODO AbstractDb.addressProvider.hdAccountIsXRandom(seedId);
+    }
+
+    public List<byte[]> signHashes(List<byte[]> hashes, List<PathTypeIndex> paths, CharSequence
+            password) throws MnemonicException.MnemonicLengthException {
+        assert hashes.size() == paths.size();
+        ArrayList<byte[]> sigs = new ArrayList<byte[]>();
+        DeterministicKey master = masterKey(password);
+        DeterministicKey account = getAccount(master);
+        DeterministicKey external = getChainRootKey(account, PathType.EXTERNAL_ROOT_PATH);
+        DeterministicKey internal = getChainRootKey(account, PathType.INTERNAL_ROOT_PATH);
+        master.wipe();
+        account.wipe();
+        for (int i = 0;
+             i < hashes.size();
+             i++) {
+            byte[] hash = hashes.get(i);
+            PathTypeIndex path = paths.get(i);
+            DeterministicKey key;
+            if (path.pathType == PathType.EXTERNAL_ROOT_PATH) {
+                key = external.deriveSoftened(path.index);
+            } else {
+                key = internal.deriveSoftened(path.index);
+            }
+            ECKey.ECDSASignature sig = key.sign(hash);
+            key.wipe();
+            sigs.add(sig.encodeToDER());
+        }
+        external.wipe();
+        internal.wipe();
+        return sigs;
+    }
+
+    public String getFirstAddressFromDb() {
+        return AbstractDb.addressProvider.getHDFristAddress(hdSeedId);
+    }
+
     @Override
     protected String getEncryptedHDSeed() {
-        return null;
+        return null;//TODO EncryptedHDSeed for HD Account Cold
     }
 
     @Override
     protected String getEncryptedMnemonicSeed() {
-        return null;
+        return null;//TODO EncryptedMnemonicSeed for HD Account Cold
+    }
+
+    private static byte[] randomByteFromSecureRandom(SecureRandom random, int length) {
+        byte[] bytes = new byte[length];
+        random.nextBytes(bytes);
+        return bytes;
     }
 }
