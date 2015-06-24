6f7e250d6e3153d42310c4afe6188a3ad9fd86d8
jjz <woaf1003@gamil.com>
2015-06-24 18:13:01 +0800

    add IProvider of coldHDAccount

---
+++ bitherj/src/main/java/net/bither/bitherj/db/IColdHDAccountAddressProvider.java
@@ -0,0 +1,25 @@
+package net.bither.bitherj.db;
+
+import java.util.List;
+
+public interface IColdHDAccountAddressProvider {
+
+
+    public int addHDAccount(String encryptedMnemonicSeed, String encryptSeed
+            , String firstAddress, boolean isXrandom, String addressOfPS
+            , byte[] externalPub, byte[] internalPub);
+
+    public String getHDFristAddress(int hdSeedId);
+
+    public byte[] getExternalPub(int hdSeedId);
+
+    public byte[] getInternalPub(int hdSeedId);
+
+    public String getHDAccountEncryptSeed(int hdSeedId);
+
+    public String getHDAccountEncryptMnmonicSeed(int hdSeedId);
+
+    public boolean hdAccountIsXRandom(int seedId);
+
+    public List<Integer> getHDAccountSeeds();
+}
---
+++ bitherj/src/main/java/net/bither/bitherj/db/IColdHDAccountProvider.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2014 http://Bither.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package net.bither.bitherj.db;
+
+import net.bither.bitherj.core.AbstractHD;
+import net.bither.bitherj.core.HDAccount;
+import net.bither.bitherj.core.In;
+import net.bither.bitherj.core.Out;
+import net.bither.bitherj.core.Tx;
+
+import java.util.HashSet;
+import java.util.List;
+
+public interface IColdHDAccountProvider {
+
+
+    public void addAddress(List<HDAccount.HDAccountAddress> hdAccountAddresses);
+
+    public int issuedIndex(AbstractHD.PathType pathType);
+
+    public int allGeneratedAddressCount(AbstractHD.PathType pathType);
+
+    public void updateIssuedIndex(AbstractHD.PathType pathType, int index);
+
+    public String externalAddress();
+
+
+    public HashSet<String> getBelongAccountAddresses(List<String> addressList);
+
+
+    public HDAccount.HDAccountAddress addressForPath(AbstractHD.PathType type, int index);
+
+    public List<byte[]> getPubs(AbstractHD.PathType pathType);
+
+    public List<HDAccount.HDAccountAddress> belongAccount(List<String> addresses);
+
+    public void updateSyncdComplete(HDAccount.HDAccountAddress address);
+
+    public void setSyncdNotComplete();
+
+    public int unSyncedAddressCount();
+
+    public void updateSyncdForIndex(AbstractHD.PathType pathType, int index);
+
+    public List<HDAccount.HDAccountAddress> getSigningAddressesForInputs(List<In> inList);
+
+    public int hdAccountTxCount();
+
+    public long getHDAccountConfirmedBanlance(int hdAccountId);
+
+    public List<Tx> getHDAccountUnconfirmedTx();
+
+    public long sentFromAccount(int hdAccountId, byte[] txHash);
+
+    public List<Tx> getTxAndDetailByHDAccount(int page);
+
+    public List<Tx> getTxAndDetailByHDAccount();
+
+    public List<Out> getUnspendOutByHDAccount(int hdAccountId);
+
+    public List<Tx> getRecentlyTxsByAccount(int greateThanBlockNo, int limit);
+
+    public int getUnspendOutCountByHDAccountWithPath(int hdAccountId, AbstractHD.PathType pathType);
+
+    public List<Out> getUnspendOutByHDAccountWithPath(int hdAccountId, AbstractHD.PathType pathType);
+}
