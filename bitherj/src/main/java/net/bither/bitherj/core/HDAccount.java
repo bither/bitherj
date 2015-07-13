8c7ff4f8cb200d6826fad46b44ba359b7342e180
ZhouQi <bitwolaiye@gmail.com>
2015-07-13 14:11:22 +0800

    do not need change

--- bitherj/src/main/java/net/bither/bitherj/core/HDAccount.java
+++ bitherj/src/main/java/net/bither/bitherj/core/HDAccount.java
@@ -277,10 +277,8 @@ public class HDAccount extends Address {
         for (int i = firstIndex;
              i < firstIndex + count;
              i++) {
-            HDAccountAddress address = new HDAccountAddress(root.deriveSoftened(i).getPubKey(), AbstractHD.PathType
-                    .INTERNAL_ROOT_PATH, i, isSyncedComplete,hdSeedId);
-            address.setHdAccountId(this.hdSeedId);
-            as.add(address);
+            as.add(new HDAccountAddress(root.deriveSoftened(i).getPubKey(), AbstractHD.PathType
+                    .INTERNAL_ROOT_PATH, i, isSyncedComplete, hdSeedId));
         }
         AbstractDb.hdAccountAddressProvider.addAddress(as);
         log.info("HD supplied {} internal addresses", as.size());
@@ -294,10 +292,8 @@ public class HDAccount extends Address {
         for (int i = firstIndex;
              i < firstIndex + count;
              i++) {
-            HDAccountAddress address = new HDAccountAddress(root.deriveSoftened(i).getPubKey(), AbstractHD.PathType
-                    .EXTERNAL_ROOT_PATH, i, isSyncedComplete,hdSeedId);
-            address.setHdAccountId(this.hdSeedId);
-            as.add(address);
+            as.add(new HDAccountAddress(root.deriveSoftened(i).getPubKey(), AbstractHD.PathType
+                    .EXTERNAL_ROOT_PATH, i, isSyncedComplete, hdSeedId));
         }
         AbstractDb.hdAccountAddressProvider.addAddress(as);
         log.info("HD supplied {} external addresses", as.size());
