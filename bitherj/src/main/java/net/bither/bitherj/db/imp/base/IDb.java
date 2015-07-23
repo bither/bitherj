0da19b6d8ded0d9c8dc9d20fb2101bf8555896d2
ZhouQi <bitwolaiye@gmail.com>
2015-07-23 13:06:24 +0800

    add interface for IDb

--- bitherj/src/main/java/net/bither/bitherj/db/imp/base/IDb.java
+++ bitherj/src/main/java/net/bither/bitherj/db/imp/base/IDb.java
@@ -16,8 +16,14 @@
 
 package net.bither.bitherj.db.imp.base;
 
+import com.google.common.base.Function;
+
 public interface IDb {
     void beginTransaction();
     void endTransaction();
     void close();
+
+    void execUpdate(String sql, String[] params);
+    void execQueryOneRecord(String sql, String[] params, Function<ICursor, Void> func);
+    void execQueryLoop(String sql, String[] params, Function<ICursor, Void> func);
 }
