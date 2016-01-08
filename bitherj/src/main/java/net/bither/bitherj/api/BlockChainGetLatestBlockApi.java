65260c4c7cfa99275cc56c03cc0b307dc780ab55
ZBK1nger <852972083@qq.com>
2016-01-08 17:57:55 +0800

    Turn Http to Https And Test Succsess.

--- bitherj/src/main/java/net/bither/bitherj/api/BlockChainGetLatestBlockApi.java
+++ bitherj/src/main/java/net/bither/bitherj/api/BlockChainGetLatestBlockApi.java
@@ -2,12 +2,13 @@ package net.bither.bitherj.api;
 
 import net.bither.bitherj.api.http.BitherUrl;
 import net.bither.bitherj.api.http.HttpGetResponse;
+import net.bither.bitherj.api.http.HttpsGetResponse;
 import net.bither.bitherj.core.Block;
 import net.bither.bitherj.utils.BlockUtil;
 
 import org.json.JSONObject;
 
-public class BlockChainGetLatestBlockApi extends HttpGetResponse<Block> {
+public class BlockChainGetLatestBlockApi extends HttpsGetResponse<Block> {
     public BlockChainGetLatestBlockApi(){
         setUrl(BitherUrl.BLOCKCHAIN_INFO_GET_LASTST_BLOCK);
     }
