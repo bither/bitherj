fb98f49ea0fd1551e293a5b106719b714dfe3e91
韩珍珍 <h_zhenzhen@126.com>
2016-10-25 13:38:45 +0800

    Modify TransactionFeeMode

--- bitherj/src/main/java/net/bither/bitherj/BitherjSettings.java
+++ bitherj/src/main/java/net/bither/bitherj/BitherjSettings.java
@@ -113,7 +113,7 @@ public class BitherjSettings {
     public static final boolean ensureMinRequiredFee = true;
 
     public enum TransactionFeeMode {
-        Normal(10000), Low(1000), High(20000);
+        Normal(10000), High(20000), Higher(50000);
 
         private int satoshi;
 
