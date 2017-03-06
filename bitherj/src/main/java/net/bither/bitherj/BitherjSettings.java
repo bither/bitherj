da1da87178bab9fc8fd56a4f44a2f26d203a075c
韩珍珍 <h_zhenzhen@126.com>
2017-03-06 18:36:15 +0800

    Add: 10x Miners Fee

--- bitherj/src/main/java/net/bither/bitherj/BitherjSettings.java
+++ bitherj/src/main/java/net/bither/bitherj/BitherjSettings.java
@@ -113,7 +113,7 @@ public class BitherjSettings {
     public static final boolean ensureMinRequiredFee = true;
 
     public enum TransactionFeeMode {
-        Normal(10000), High(20000), Higher(50000);
+        Normal(10000), High(20000), Higher(50000), TenX(100000);
 
         private int satoshi;
 
