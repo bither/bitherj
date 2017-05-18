b97c39faca656518a798abc9821caa339e032e5d
zhumingu <zhumingu@163.com>
2017-05-18 20:55:40 +0800

    fix: version code

--- bitherj/src/main/java/net/bither/bitherj/core/Version.java
+++ bitherj/src/main/java/net/bither/bitherj/core/Version.java
@@ -17,6 +17,6 @@
 package net.bither.bitherj.core;
 
 public class Version {
-    public static final String version = "1.4.3";
+    public static final String version = "1.4.4";
     public static final String name = "Bitherj";
 }
