936c0799e9b9d6f9af8ed1294e4608c3a147cd2b
hanzhenzhen <hanzhenzhen666@gmail.com>
2018-09-06 19:31:02 +0800

    add segwit tx display

--- bitherj/src/main/java/net/bither/bitherj/core/Tx.java
+++ bitherj/src/main/java/net/bither/bitherj/core/Tx.java
@@ -529,6 +529,13 @@ public class Tx extends Message implements Comparable<Tx> {
         // First come the inputs.
         long numInputs = readVarInt();
         optimalEncodingMessageSize += VarInt.sizeOf(numInputs);
+        if (numInputs == 0) {
+            if (readVarInt() == 1) {
+                optimalEncodingMessageSize += 1;
+                numInputs = readVarInt();
+                optimalEncodingMessageSize += VarInt.sizeOf(numInputs);
+            }
+        }
         this.ins = new ArrayList<In>((int) numInputs);
         for (int i = 0;
              i < numInputs;
@@ -555,6 +562,7 @@ public class Tx extends Message implements Comparable<Tx> {
             optimalEncodingMessageSize += 8 + VarInt.sizeOf(scriptLen) + scriptLen;
             cursor += scriptLen;
         }
+        cursor = bytes.length - 4;
         this.txLockTime = readUint32();
         optimalEncodingMessageSize += 4;
         this.length = cursor - offset;
